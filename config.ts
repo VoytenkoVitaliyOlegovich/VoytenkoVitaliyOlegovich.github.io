45b25351e890d47e0113d9283a4bd8054b8d09ea
Vitaliy <hot1ce1odessa@gmail.com>
2021-06-07 22:39:57 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,29 @@
 import IConfig from './src/interfaces/IConfig';
 
 export default {
-  //
-} as IConfig;
+  services: {
+    github: {
+      configuration: {
+        nickname: 'VoytenkoVitaliyOlegovich',
+        sort: {
+          repositories: [
+            { attr: 'created_at', sortByDesc: true },
+            { attr: 'stargazers_count', sortByDesc: true },
+          ],
+        },
+        filter: {
+          repositories: [
+            [
+              { attr: 'owner.login', values: ['Alexeykhr', 'GPortfolio', 'uSchedule', 'uRepairPC'] },
+              { attr: 'name', values: ['awesome-vue', 'merge-conflict', 'github-pages-with-jekyll', 'markdown-portfolio'], revert: true },
+              { attr: 'fork', values: false }
+            ],
+          ],
+        },
+      },
+    },
+  },
+  data: {
+    position: 'Front-end Developer',
+  },
+} as IConfig
